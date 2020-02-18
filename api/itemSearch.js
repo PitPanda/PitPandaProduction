4ceb0b662295801daa0ca916d30132fe06f4c2c7
McPqndq <kellybob3737@gmail.com>
2020-02-17 19:45:28 -0600

    minor fixes pt2 lol

--- api/itemSearch.js
+++ api/itemSearch.js
@@ -5,13 +5,15 @@ const {dbToItem} = require('./apiTools');
 const router = require('express').Router();
 router.use('/:query', (req,res)=>{
     const query = req.params.query.split(',').map(str=>{
-        const end = /[0-9]/.exec(str);
+        const end = /[0-9]{1,}$/.exec(str);
         if(end) return {
-            key: str.substring(0,end),
-            level: Number(str.substring(end))
+            key: str.substring(0,end.index),
+            level: Number(str.substring(end.index))
         }
         else return{};
     });
+    query.forEach(q=>console.log(typeof q.key,typeof q.level));
+    console.log(query);
     if(!query.every(q=>typeof q.key === 'string' && typeof q.level === 'number')) return res.status(400).send({error:'invalid query'})
     Mystic.find({
         enchants:{
