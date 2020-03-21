3e2cb2e1ac1faa0a6b187b66936eeeb8269d9877
McPqndq <kellybob3737@gmail.com>
2020-03-21 18:19:44 -0500

    hotfixing in production time

--- routes/Username.js
+++ routes/Username.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const getUsername = require('../apiTools/playerDocRequest');
+const getDoc = require('../apiTools/playerDocRequest');
 
 router.get('/:uuid', async (req, res) => {
-    const user = await getUsername(req.params.uuid);
+    const user = await getDoc(req.params.uuid);
     if (user.error) return res.status(400).json({ success: false, error: user.error });
 
     res.status(200).json({ success: true, name: user.displayName });
