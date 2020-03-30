3caf16191aee34bcfe2cbcd37e324d09487bd4d1
McPqndq <kellybob3737@gmail.com>
2020-03-29 21:15:31 -0500

    patched color command

--- discordBot/commands/color.js
+++ discordBot/commands/color.js
@@ -1,31 +1,23 @@
 const TradeCenter = require('../TradeCenter.json');
 const Command = require('../Command');
 
-function command(msg,rest){
-    const color = rest[0];
+const command = async (msg,rest) => {
+    const color = rest[0].toUpperCase().split('').filter(c=>/^[0-9A-F]$/.test(c)).join('');
     const roleManager = msg.member.roles;
-    if(/^#?[0-9a-fA-F]{6}$/.test(color)){
-        if(color.startsWith('#'))color=color.substring(1);
-        const resolve = (role) => {
-            roleManager.add(role,'Color Bot Role');
-            msg.reply('Your role has been successfully added!');
-        }
-        let roles = roleManager.cache.filter(role=>/^#[0-9a-fA-F]{6}$/.test(role.name));
-        roleManager.remove(roles,'Duplicate/Old Color Role').then(()=>{
-            Promise.all(roles.filter(role=>role.members.array().length===0).map(role=>role.delete("Role has no remaining members"))).then(()=>{
-                const role = msg.guild.roles.cache.find(p=>p.name===`#${color}`);
-                if(role) resolve(role);
-                else msg.guild.roles.create({
-                    data:{
-                        name: `#${color}`,
-                        color: color,
-                        position: msg.guild.roles.cache.get(TradeCenter.HexMarker).position-2
-                    },
-                    reason:"Color role"
-                }).then(resolve);
-            });
-        });
-    } else msg.reply('Please give a color in hex format ex: #00ff00');
+    if(!(/^#?[0-9a-fA-F]{6}$/.test(color))) return msg.reply('Please give a color in hex format ex: #00ff00');
+    const roles = roleManager.cache.filter(role=>/^#[0-9a-fA-F]{6}$/.test(role.name));
+    await roleManager.remove(roles,'Duplicate/Old Color Role');
+    await Promise.all(roles.filter(role=>role.members.array().length===0).map(role=>role.delete("Role has no remaining members")))
+    const role = msg.guild.roles.cache.find(p=>p.name===`#${color}`) || await msg.guild.roles.create({
+        data:{
+            name: `#${color}`,
+            color: color,
+            position: msg.guild.roles.cache.get(TradeCenter.HexMarker).position-2,
+        },
+        reason:"Color role",
+    })
+    roleManager.add(role,'Color Bot Role');
+    msg.reply('Your role has been successfully added!');
 }
 
 module.exports = new Command({
