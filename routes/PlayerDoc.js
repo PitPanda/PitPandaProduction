a781eb761fefb244a94df1e7438c3365daa4821c
McPqndq <kellybob3737@gmail.com>
2020-04-17 10:50:47 -0500

    playerdoc age param

--- routes/PlayerDoc.js
+++ routes/PlayerDoc.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const getDoc = require('../apiTools/playerDocRequest');
 
 router.get('/:uuid', async (req, res) => {
-    const user = await getDoc(req.params.uuid, (req.query.age||86400)*1e3);
+    const user = await getDoc(req.params.uuid, {maxAge:(req.query.age||86400)*1e3});
     if (user.error) return res.status(400).json({ success: false, error: user.error });
 
     res.status(200).json({ success: true, Doc: user });
