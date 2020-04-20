f59dd0cc267606945a50ea8476c808117df41f3d
McPqndq <kellybob3737@gmail.com>
2020-04-20 14:27:50 -0500

    page size option for lb endpoint

--- routes/Leaderboard.js
+++ routes/Leaderboard.js
@@ -3,7 +3,7 @@ const { APIerror } = require('../apiTools/apiTools');
 const leaderboardGrabber = require('../apiTools/leaderboardGrabber');
 
 router.get('/:type', async (req, res) => {
-    const leaderboard = await leaderboardGrabber(req.params.type, req.query.page || 0);
+    const leaderboard = await leaderboardGrabber(req.params.type, req.query.page || 0, req.query.pageSize);
 
     if (leaderboard.error) return res.status(400).json({ success: false, error: leaderboard.error||'Something went wrong.' });
 
