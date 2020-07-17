788508e918e45f09ca3de1bd9ac96a56ed6f4502
McPqndq <kellybob3737@gmail.com>
2020-07-17 01:09:21 -0600

    removed error check lol

--- routes/DiscordToDocs.js
+++ routes/DiscordToDocs.js
@@ -3,7 +3,6 @@ const Player = require('../models/Player');
 
 router.get('/:tag', async (req, res) => {
     const users = await Player.find({discord:req.params.tag});
-    if (user.error) return res.status(400).json({ success: false, error: user.error });
 
     res.status(200).json({ success: true, users});
 });
