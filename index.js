87f57994a0baf164d46141d93d3d8837ea39d975
McPqndq <kellybob3737@gmail.com>
2020-07-22 01:25:56 -0500

    low effort rate limiting part1

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const expressWs = require('express-ws')(app);
 const api = require('./routes');
 const mongoose = require('mongoose');
+const fs = require('fs');
 const {dbLogin, Development} = require('./settings.json');
+const RedisClient = new (require('./utils/RedisClient'))(0);
 require('./discordBot');
 
 mongoose.connect(dbLogin, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('MongoDB Connected'));
@@ -11,9 +13,24 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('autoIndex', Development);
 
+app.set('trust proxy', true);
+
 app.use(express.static('frontEnd/build'));
 
-app.use('/api', api);
+const rateLimitManager = fs.readFileSync('./redis/scripts/rateLimitManager.lua', {encoding: 'utf-8'});
+
+app.use('/api', async (req, res, next) => {
+  let token = `rl:ip:${req.ip}`;
+  let limit = 1;
+  const [err, used] = await new Promise(resolve=>RedisClient.client.eval(rateLimitManager, 1, token, Math.floor(Date.now()/1e3), 60, limit, 1, (err, used)=>resolve([err,used])));
+  if(err) {
+    console.error(err);
+    console.log(used);
+    return res.status(500).send({ success: false, error: err });
+  }
+  if(used>=limit) return res.status(429).send({ success: false, error: 'Rate Limited' });
+  next();
+}, api);
 app.use('/pitReference', (req, res) => res.status(200).sendFile(__dirname + "/frontEnd/src/pitMaster.json"));
 
 app.ws('*', (ws,req)=>ws.send('Invalid websocket endpoint'))
---
+++ redis/scripts/rateLimitManager.lua
@@ -0,0 +1,27 @@
+local token = KEYS[1]
+local now = tonumber(ARGV[1])
+local window = tonumber(ARGV[2])
+local limit = tonumber(ARGV[3])
+local cost = tonumber(ARGV[4])
+
+redis.call('EXPIRE', token, window);
+
+local clearBefore = now - window
+local keys = redis.call('HGETALL', token)
+local len = table.getn(keys)
+local used = 0
+for i = 1,len,2 do
+    local stamp = tonumber(keys[i])
+    if stamp < clearBefore then
+        redis.call('HDEL', token, keys[i])
+    else
+        used = used + tonumber(keys[i+1])
+    end
+end
+
+if used + cost <= limit then
+    redis.call('HINCRBY', token, now, cost)
+    used = used + cost
+end
+
+return used
