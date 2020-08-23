901f02b1d09d693a688a5d60e7bf1dd4c6e6f2c0
McPqndq <kellybob3737@gmail.com>
2020-08-23 17:41:21 -0500

    Update PlayerDoc.js

--- routes/PlayerDoc.js
+++ routes/PlayerDoc.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const getDoc = require('../apiTools/playerDocRequest');
 const rateLimiter = require('../apiTools/rateLimiter');
 router.get('/:uuid', rateLimiter(3), async (req, res) => {
-    const maxAge = (req.query.age ? Math.max(Number(req.params.maxAge),3600) : 86400) * 1e3;
+    const maxAge = (req.query.maxAge ? Math.max(Number(req.params.maxAge),900) : 86400) * 1e3;
     const user = await getDoc(req.params.uuid, {maxAge});
     if (user.error) return res.status(400).json({ success: false, error: user.error });
 
