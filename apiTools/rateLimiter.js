b1ab8d573c36a004452db281c9319d1c5150769d
McPqndq <kellybob3737@gmail.com>
2020-09-10 16:06:53 -0500

    allow passing api key as header

--- apiTools/rateLimiter.js
+++ apiTools/rateLimiter.js
@@ -4,15 +4,16 @@ const RedisClient = new (require('../utils/RedisClient'))(0);
 const rateLimitManager = fs.readFileSync('./redis/scripts/rateLimitManager.lua', {encoding: 'utf-8'});
 
 module.exports = cost => async (req, res, next) => {
-  let token = `rl:ip:${req.ip}`
-  let limit = 120
-  if(req.query.key){
+  let token = `rl:ip:${req.ip}`;
+  let limit = 120;
+  const passed = req.query.key || req.get("X-API-Key");
+  if(passed){
     try{
-      limit = await new Promise((resolve, reject) => RedisClient.client.hget(`apikey:${req.query.key}`, 'limit', (err, limit) => {
+      limit = await new Promise((resolve, reject) => RedisClient.client.hget(`apikey:${passed}`, 'limit', (err, limit) => {
         if(err) reject(err);
         resolve(Number(limit));
       }));
-      token = `rl:key:${req.query.key}`;
+      token = `rl:key:${passed}`;
     }catch(e){ }
   }
   const [err, used] = await new Promise(resolve=>RedisClient.client.eval(rateLimitManager, 1, token, Math.floor(Date.now()/1e3), 60, limit, cost, (err, used)=>resolve([err,used])));
