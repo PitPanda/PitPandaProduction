220b9fb913c8f5ac8dcd9c05f022b798de2a0da0
McPqndq <kellybob3737@gmail.com>
2020-09-20 00:21:45 -0500

    typo is newmystic socket

--- routes/NewMystics.js
+++ routes/NewMystics.js
@@ -7,8 +7,8 @@ events.on('new', console.log)
 router.ws('/', (ws) => {
     const cb = item => ws.send(JSON.stringify(item));
     events.on('new', cb);
-    ws.on('close', events.removeListener('new', cb));
-    ws.on('message', ()=>ws.send('3'));
+    ws.on('close', () => events.removeListener('new', cb));
+    ws.on('message', () => ws.send('3'));
 });
 
 router.use('/', APIerror('This is a websocket only endpoint'));
