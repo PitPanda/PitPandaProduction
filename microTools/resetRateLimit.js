1c2aaab9b775272776826516df88770cf229c83d
McPqndq <kellybob3737@gmail.com>
2021-03-19 16:28:50 -0500

    redis clean tool

---
+++ microTools/resetRateLimit.js
@@ -0,0 +1,13 @@
+require('../setup');
+
+const redis = new (require('../utils/RedisClient'))(0);
+
+const scanHandler = (err, [cursor, keys]) => {
+  for(const key of keys){
+    redis.client.del(key);
+  }
+  if(cursor != 0) redis.client.scan(cursor, 'MATCH', 'rl:*', scanHandler)
+}
+
+redis.client.scan(0, 'MATCH', 'rl:*', scanHandler)
+
