77b7f6fb6bcdbb39a3ede0d886a70166f1e76a2c
McPqndq <kellybob3737@gmail.com>
2021-03-18 21:28:38 -0500

    Update KeyInfo.js

--- routes/KeyInfo.js
+++ routes/KeyInfo.js
@@ -5,7 +5,9 @@ const rateLimiter = require('../apiTools/rateLimiter');
 router.get("/", rateLimiter(1, true), async (req, res) => {
   redis.client.hgetall(`apikey:${req.apiKey}`, (err, reply) => {
     if(err || Object.keys(reply).length === 0) res.status(400).send({ success: false });
-    res.send({success: true, limit: Number(reply.limit), owner: reply.owner })
+    redis.client.hgetall(`keyof:${reply.owner}`, (err2, reply2) => {
+      res.send({success: true, limit: Number(reply.limit), owner: reply.owner, uses: reply2.uses });
+    });
   });
 });
 
