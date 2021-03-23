6145bae25b3a9e55bc166a5043e829cf0e85720b
McPqndq <kellybob3737@gmail.com>
2021-03-22 20:36:42 -0500

    /keyinfo changes
    allow checking on a key other than the one used to make the request

--- routes/KeyInfo.js
+++ routes/KeyInfo.js
@@ -3,7 +3,8 @@ const redis = new (require('../utils/RedisClient'))(0);
 const rateLimiter = require('../apiTools/rateLimiter');
 
 router.get("/", rateLimiter(1, true), async (req, res) => {
-  redis.client.hgetall(`apikey:${req.apiKey}`, (err, reply) => {
+  const checking = req.query.checkkey || req.apiKey;
+  redis.client.hgetall(`apikey:${checking}`, (err, reply) => {
     if(err || Object.keys(reply).length === 0) res.status(400).send({ success: false });
     redis.client.hgetall(`keyof:${reply.owner}`, (err2, reply2) => {
       res.send({success: true, limit: Number(reply.limit), owner: reply.owner, uses: Number(reply2.uses) });
