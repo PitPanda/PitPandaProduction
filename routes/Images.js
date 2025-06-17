4987e2d3fbed1c324db2af4539d6c29603c3b0b6
Brooke Gill <brokegill@gmail.com>
2025-06-16 18:55:30 -0700

    Hotfix for fixing image API

--- routes/Images.js
+++ routes/Images.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const proxy = require('express-http-proxy');
 
-router.use(proxy('localhost:5002'))
+const imageApiUrl = process.env.IMAGE_API_URL || 'localhost:5002';
+
+router.use(proxy(imageApiUrl))
 
 module.exports = router;
